d49f001b3c02e1c94ca30af32b7d82ff441a5812
GalH69 <auto.install@cs.com>
2025-08-07 00:44:11 +0300

    'autosave'

--- server.py
+++ server.py
@@ -64,6 +64,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         file_size_bytes = conn.recv(4)
                         file_size = int.from_bytes(file_size_bytes, byteorder='big')
 
+                        
+
+
                         received = 0
                         while received < file_size:
                             chunk = conn.recv(min(1024, file_size - received))
