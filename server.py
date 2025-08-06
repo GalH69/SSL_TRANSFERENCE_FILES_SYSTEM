d993304319e55ec208eb357c8a6bc8ba3cc38c26
GalH69 <auto.install@cs.com>
2025-08-06 22:58:16 +0300

    'autosave'

--- server.py
+++ server.py
@@ -41,7 +41,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                             conn.sendall(b"Good name!")
                             break
                         
-                    target_file_name = f"received_{file_name}"
+                    target_file_name = os.path.join(folder, f"received_{file_name}")
                     with open(target_file_name, "wb") as rf:
                         # buffer = b""
                         # while True:
